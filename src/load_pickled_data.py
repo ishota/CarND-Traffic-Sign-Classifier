6339977774bab2aef7d72f9860f37693d7a9c30e
ishota <ishota1991@gmail.com>
2019-11-11 07:37:26 +0900

    update summary_data.py via upload

--- src/load_pickled_data.py
+++ src/load_pickled_data.py
@@ -24,6 +24,6 @@ def load_pickled_data(data_path):
 
 
 if __name__ == '__main__':
-    print("test: load_pickled_data")
+    print("[test]: load_pickled_data")
     path = str(Path('.').resolve().parents[0]) + os.sep + 'traffic-signs-data'
     X_train, y_train, X_valid, y_valid, X_test, y_test = load_pickled_data(path)
