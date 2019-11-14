4cbf09ec87eefbf6f4da842216086ebcbfffd5bb
ishota <ishota1991@gmail.com>
2019-11-15 08:15:49 +0900

    add proposed 10 layers model

--- src/traffic_sign_classifier.py
+++ src/traffic_sign_classifier.py
@@ -7,7 +7,7 @@ from summary_data import *
 from nn_model import *
 
 
-def main():
+def basic_lenet():
 
     # load_pickled_data
     data_path = str(Path('.').resolve().parents[0]) + os.sep + 'traffic-signs-data'
@@ -32,5 +32,31 @@ def main():
     print('test_accuracy: ' + str(test_acc))
 
 
+def main():
+
+    # load_pickled_data
+    data_path = str(Path('.').resolve().parents[0]) + os.sep + 'traffic-signs-data'
+    X_train, y_train, X_valid, y_valid, X_test, y_test = load_pickled_data(data_path)
+
+    # summary_data
+    csv_file = str(Path('.').resolve().parents[0]) + os.sep + 'signnames.csv'
+    n_classes = summary_data(X_train, y_train, X_valid, y_valid, X_test, y_test, csv_file)
+
+    # create the model
+    model = proposed()
+
+    # compile the model
+    model.compile(optimizer='RMSprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # train the model
+    history = model.fit(X_train, y_train, epochs=10, validation_data=(X_valid, y_valid))
+
+    # evaluate the model
+    test_loss, test_acc = model.evaluate(X_test, y_test, verbose=0)
+    print('test_loss: ' + str(test_loss))
+    print('test_accuracy: ' + str(test_acc))
+
+
 if __name__ == '__main__':
+    # basic_lenet()
     main()
